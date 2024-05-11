8f31a7cf2020f1e90014b4d2084eb7ef17a79811
shivamrai27 <200shivamrai@gmail.com>
2024-05-11 08:17:44 +0530

    change in url

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shopfever-api.vercel.app/api/',
+    baseUrl: 'https://shopfever-api.vercel.app/',
     credentials: 'include'
   }),
   endpoints: (builder) => ({
